040170121dfbbfb2620aa9740500a04b7c9c46d5
Nekroarteriaa <Nekroarteriaa@gmail.com>
2020-11-05 17:05:31 +0100

    Refactoring code

---
+++ Assets/Scripts/MovementScripts/MovementRigidbodyBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovementRigidbodyBehaviour : MovementBase
+{
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        Vector3 mousePosition = GetCurrentMousePoisition();
+
+        Move(mousePosition.x, mousePosition.y);
+    }
+    
+
+    public override void Move(float horizontal, float vertical, float depth)
+    {
+        var direction = new Vector3(horizontal, vertical, 0);
+        var projectileDelta = direction - transform.position;
+        rigidbody.AddForce(projectileDelta.normalized * speedDataSO.Speed);
+    }
+
+    private Vector3 GetCurrentMousePoisition()
+    {
+        var mouseInput = Input.mousePosition;
+        mouseInput.z = 10;
+        var mousePosition = Camera.main.ScreenToWorldPoint(mouseInput);
+        mousePosition.z = 0;
+        return mousePosition;
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/SpeedDataSO.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpeedDataSO", menuName = "Data/SpeedSO")]
+public class SpeedDataSO : ScriptableObject
+{
+    [SerializeField]
+    private float speed;
+
+    public float Speed
+    {
+        get { return speed; }
+        private set { speed = value; }
+    }
+}
